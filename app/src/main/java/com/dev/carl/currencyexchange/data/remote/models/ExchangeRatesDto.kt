a328b2a99809d218205cbae57d2f9a11b3c3c707
clv <dev.carllouissevisda@gmail.com>
2025-04-03 11:51:44 +0800

    Update mappers and data classes to address parsing issue

--- app/src/main/java/com/dev/carl/currencyexchange/data/remote/models/ExchangeRatesDto.kt
+++ app/src/main/java/com/dev/carl/currencyexchange/data/remote/models/ExchangeRatesDto.kt
@@ -3,10 +3,7 @@ package com.dev.carl.currencyexchange.data.remote.models
 import com.squareup.moshi.Json
 
 data class ExchangeRatesDto(
-    @field:Json(name = "base")
-    val base: String,
-    @field:Json(name = "date")
-    val date: String,
-    @field:Json(name = "rates")
-    val rates: RatesDto
+    @field:Json(name = "base") val base: String,
+    @field:Json(name = "date") val date: String,
+    @field:Json(name = "rates") val rates: Map<String, Double>
 )
--- app/src/main/java/com/dev/carl/currencyexchange/ui/converter/ExchangeState.kt
+++ app/src/main/java/com/dev/carl/currencyexchange/ui/converter/ExchangeState.kt
@@ -1,9 +1,19 @@
 package com.dev.carl.currencyexchange.ui.converter
 
+import com.dev.carl.currencyexchange.common.Constants.DEFAULT_CURRENCY
+import com.dev.carl.currencyexchange.domain.models.Balances
 import com.dev.carl.currencyexchange.domain.models.ExchangeRates
+
+val DEFAULT_BALANCES = mutableListOf(Balances(amount = 1000.00, currency = DEFAULT_CURRENCY))
 
 data class ExchangeState(
     val ratesData: ExchangeRates? = null,
+    val fromCurrency: String = DEFAULT_CURRENCY,
+    val toCurrency: String = DEFAULT_CURRENCY,
+    val balances: MutableList<Balances> = DEFAULT_BALANCES.toMutableList(),
+    val exchangeCount: Int = 0,
+    val conversionDetails: String = "",
+    val lastConvertedAmount: Double = 0.0,
     val isLoading: Boolean = false,
-    val error: String? = null
+    val error: String? = null,
 )
